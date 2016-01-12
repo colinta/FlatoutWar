3f1e2c204a4ec0cf99a347d59d4be8e5d3809b6e
Colin T.A. Gray <colinta@gmail.com>
2016-01-12 16:38:40 -0700

    dynamic number of flying targets

--- Source/Game/Components/FlyingComponent.swift
+++ Source/Game/Components/FlyingComponent.swift
@@ -15,14 +15,14 @@ class FlyingComponent: RammingComponent {
     override var currentTarget: Node? {
         didSet {
             if let currentTarget = currentTarget {
-                let numTargets: Int = 2
                 var points: [CGPoint] = []
                 let dist = currentTarget.distanceTo(node)
+                let numTargets: Int = Int(dist / 200)
                 let nodeAngle = currentTarget.angleTo(node)
                 let segment = dist / CGFloat(numTargets + 1)
                 for i in 0..<numTargets {
                     let radius = CGFloat(numTargets - i) * segment ± rand(segment / 4)
-                    let angle = nodeAngle ± rand((7.5 * CGFloat(numTargets - i)).degrees)
+                    let angle = nodeAngle ± rand(15.degrees)
                     points << (currentTarget.position + CGPoint(r: radius, a: angle))
                 }
                 flyingTargets = points
