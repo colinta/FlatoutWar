9a864262a7d531b1391168db189da3ccdba3bc06
Colin T.A. Gray <colinta@gmail.com>
2016-01-28 10:58:04 -0700

    adds EnemyJetNodes to MainMenu

--- Source/Game/Worlds/UI/MainMenuWorld.swift
+++ Source/Game/Worlds/UI/MainMenuWorld.swift
@@ -15,13 +15,64 @@ class MainMenuWorld: World {
         textNode.text = "FLATOUT WAR"
         self << textNode
 
-        let enemyNode = EnemySoldierNode(at: CGPoint(x: -200, y: 0))
-        let wanderingComponent = WanderingComponent()
-        wanderingComponent.wanderingRadius = 50
-        wanderingComponent.centeredAround = enemyNode.position
-        wanderingComponent.maxSpeed = 10
-        enemyNode.addComponent(wanderingComponent)
-        self << enemyNode
+        let howMany: Int = rand(8...15)
+        howMany.times {
+            let enemyNode = EnemySoldierNode(
+                at: CGPoint(
+                    x: rand(min: -size.width / 2, max: size.width / 2) as CGFloat,
+                    y: rand(min: -size.height / 2, max: size.height / 2) as CGFloat
+                )
+            )
+            let wanderingComponent = WanderingComponent()
+            wanderingComponent.wanderingRadius = 50
+            wanderingComponent.centeredAround = enemyNode.position
+            wanderingComponent.maxSpeed = 10
+            enemyNode.addComponent(wanderingComponent)
+            self << enemyNode
+        }
+
+        timeline.every(5...7, startAt: 1) {
+            let startAngle: CGFloat = rand(TAU)
+            let flyAngle: CGFloat = startAngle + TAU_2 ± rand(TAU_16)
+            let velocity = CGPoint(r: 100, a: flyAngle)
+
+            let dist: CGFloat = 12
+            let center = self.outsideWorld(extra: dist * 2 + 1, angle: startAngle)
+            let left = CGVector(r: dist, a: flyAngle + TAU_4)
+            let right = CGVector(r: dist, a: flyAngle - TAU_4)
+            let back = CGVector(r: dist * 2, a: flyAngle)
+
+            let origins: [CGPoint] = [
+                center,
+                center + left,
+                center + right,
+                center + back,
+                center + back + left,
+                center + back + right,
+                center - back,
+                center - back + left,
+                center - back + right,
+            ]
+            for origin in origins {
+                let enemy = EnemyJetNode(at: origin)
+                enemy.name = "jet"
+                enemy.rotateTo(flyAngle)
+                self << enemy
+
+                enemy.flyingComponent!.maxSpeed = velocity.length
+                enemy.flyingComponent!.maxTurningSpeed = velocity.length
+                enemy.flyingComponent!.currentSpeed = velocity.length
+                enemy.flyingComponent!.flyingTargets = [
+                    center + CGVector(r: self.outerRadius / 2, a: flyAngle) + CGVector(r: ±rand(25), a: flyAngle + TAU_4),
+                    center + CGVector(r: self.outerRadius, a: flyAngle) + CGVector(r: ±rand(25), a: flyAngle + TAU_4),
+                    center + CGVector(r: self.outerRadius * 2, a: flyAngle) + CGVector(r: ±rand(25), a: flyAngle + TAU_4),
+                ]
+
+                self.timeline.after(7) {
+                    enemy.removeFromParent()
+                }
+            }
+        }
 
         let startButtonNode = Button(at: CGPoint(x: 0, y: 0))
         startButtonNode.text = "START"
