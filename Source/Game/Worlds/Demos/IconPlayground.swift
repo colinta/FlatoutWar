ebda8793632b982a4211568a952a813517bcb7e1
Colin T.A. Gray <colinta@gmail.com>
2016-06-29 09:18:15 -0600

    cleared Playground, created Icon and Orbital Playgrounds

---
+++ Source/Game/Worlds/Demos/IconPlayground.swift
@@ -0,0 +1,63 @@
+//
+//  IconPlayground.swift
+//  FlatoutWar
+//
+//  Created by Colin Gray on 12/21/2015.
+//  Copyright (c) 2015 FlatoutWar. All rights reserved.
+//
+
+class IconPlayground: World {
+
+    override func populateWorld() {
+        moveCamera(zoom: 2, duration: 0.1)
+
+        let playerNode = BasePlayerNode()
+        playerNode.firingComponent?.enabled = false
+        playerNode.rotateTo(35.degrees)
+        self << playerNode
+
+        do {
+            let enemy = EnemySoldierNode()
+            enemy.position = CGPoint(x: 40, y: 30)
+            enemy.rotateTowards(playerNode)
+            enemy.active = false
+            self << enemy
+
+            enemy.sprite.lightingBitMask   = 0xFFFFFFFF
+            enemy.sprite.shadowCastBitMask = 0xFFFFFFFF
+        }
+
+        do {
+            let enemy = EnemySoldierNode()
+            enemy.position = CGPoint(x: 40, y: 0)
+            enemy.rotateTowards(playerNode)
+            enemy.active = false
+            self << enemy
+        }
+
+        // do {
+        //     let enemy = EnemyBigJetNode()
+        //     enemy.position = CGPoint(x: -25, y: 25)
+        //     enemy.rotateTo(-TAU_3_8)
+        //     enemy.active = false
+        //     self << enemy
+        // }
+
+        // do {
+        //     let enemy = EnemyJetNode()
+        //     enemy.position = CGPoint(x: -17, y: 33)
+        //     enemy.rotateTo(-TAU_3_8)
+        //     enemy.active = false
+        //     self << enemy
+        // }
+
+        // do {
+        //     let enemy = EnemyJetNode()
+        //     enemy.position = CGPoint(x: -11, y: 39)
+        //     enemy.rotateTo(-TAU_3_8)
+        //     enemy.active = false
+        //     self << enemy
+        // }
+    }
+
+}
--- Source/Game/Worlds/Demos/Playground.swift
+++ Source/Game/Worlds/Demos/Playground.swift
@@ -7,44 +7,14 @@
 //
 
 class Playground: World {
-    var time: CGFloat = 0
-    var sprites: [(SKNode, SKSpriteNode, a: CGFloat, offset: CGFloat, factor: CGFloat)] = []
 
     override func populateWorld() {
-        20.times {
-            sprites << (SKNode(), SKSpriteNode(id: .Box(color: EnemySoldierGreen)), a: rand(TAU), offset: rand(TAU), factor: rand(min: 1, max: 3))
-        }
-
-        for (node, sprite, angle, _, _) in sprites {
-            node << sprite
-            node.zRotation = angle
-            self << node
-        }
-    }
-
-    func p(t: CGFloat, r: CGFloat = 30) -> CGPoint {
-        let x = r * (sin(t) + sin(-2 * t) * 0.5 - sin(4 * t))
-        let y = r * (cos(t) + cos(-2 * t) * 0.5 - cos(4 * t))
-        return CGPoint(x, y)
     }
 
     override func worldShook() {
-        timeRate /= 2
-        if timeRate < 0.125 {
-            timeRate = 8
-        }
     }
 
     override func update(dt: CGFloat) {
-        time += dt
-
-        for (_, sprite, _, offset, factor) in sprites {
-            let t = factor * (time - offset * TAU)
-            let p0 = p(t)
-            let p1 = p(t + dt)
-            sprite.position = p0
-            sprite.zRotation = p0.angleTo(p1)
-        }
     }
 
     func letters() {
