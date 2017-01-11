f53dce858103417543d135c84ede12700204887e
Colin T.A. Gray <colinta@gmail.com>
2017-01-11 16:30:33 -0700

    update exp in level 2

--- Source/Game/Worlds/Config/TutorialLevel2Config.swift
+++ Source/Game/Worlds/Config/TutorialLevel2Config.swift
@@ -3,7 +3,7 @@
 //
 
 class TutorialLevel2Config: TutorialConfig {
-    override var possibleExperience: Int { return 110 }
+    override var possibleExperience: Int { return 115 }
 
     override func tutorial() -> Tutorial? { return nil }
     override func nextLevel() -> Level {
