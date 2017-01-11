1ee8ce565277a093ad7e9bb85f52bc6fea5487e2
Colin T.A. Gray <colinta@gmail.com>
2017-01-11 16:30:20 -0700

    don't limit mines distance from player

--- Source/Game/Powerups/MinesPowerup.swift
+++ Source/Game/Powerups/MinesPowerup.swift
@@ -13,26 +13,15 @@ class MinesPowerup: Powerup {
     override func activate(level: World, layer: SKNode, playerNode: Node, completion: @escaping Block = {}) {
         super.activate(level: level, layer: layer, playerNode: playerNode)
 
-        self.onNextTap(slowmo: true) { tapPosition in
-            let position: CGPoint
-            if tapPosition.distanceTo(playerNode.position, within: 100)
-                && !tapPosition.distanceTo(playerNode.position, within: 20)
-            {
-                position = tapPosition
-            }
-            else {
-                let a = tapPosition.angle
-                let r = min(100, max(20, tapPosition.distanceTo(playerNode.position)))
-                position = CGPoint(r: r, a: a)
-            }
-
+        self.onNextTap(slowmo: true) { position in
             let moveDuration: CGFloat = 0.1
             5.times { (i: Int) in
-                let a: CGFloat = CGFloat(i) * TAU / 5 ± rand(TAU_16)
+                let a: CGFloat = CGFloat(i) * TAU / 5 ± rand(weighted: TAU_16)
                 let r: CGFloat = 17
-                let offset = position + CGPoint(r: r ± rand(3), a: a)
-                let node = MineNode(at: playerNode.position)
-                node.moveTo(offset, duration: moveDuration)
+                let offset = CGPoint(r: r + rand(weighted: 3), a: a)
+                let node = MineNode(at: position)
+                node.moveTo(position + offset, duration: moveDuration)
+                node.scaleTo(1, start: 0, duration: moveDuration)
                 layer << node
             }
             level.timeline.after(time: moveDuration) {
