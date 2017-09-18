86b7b08274b98203df9f328d0e9e401b9208c59f
Colin T.A. Gray <colinta@gmail.com>
2017-09-18 09:55:59 -0600

    move powerup text code

--- Source/Game/Worlds/03-Ocean/OceanLevel1.swift
+++ Source/Game/Worlds/03-Ocean/OceanLevel1.swift
@@ -19,34 +19,41 @@ class OceanLevel1: OceanLevel {
     }
 
     override func populateZoomOut() {
-        let textNode = TextNode()
+        let guardTextNode = TextNode()
         timeline.after(time: 0.5) {
-            textNode.text = "SEPTENTRION\nGUARDS\n\nWORK BEST\nIN GROUPS."
-            textNode.position = CGPoint(50, 0)
-            textNode.fadeTo(1, start: 0, duration: 0.5)
-            self.gameUI << textNode
+            guardTextNode.text = "SEPTENTRION\nGUARDS\n\nWORK BEST\nIN GROUPS."
+            guardTextNode.position = CGPoint(50, 0)
+            guardTextNode.fadeTo(1, start: 0, duration: 0.5)
+            self.gameUI << guardTextNode
         }
 
         timeline.after(time: 3.5) {
-            textNode.fadeTo(0, duration: 0.5, removeNode: true)
+            guardTextNode.fadeTo(0, duration: 0.5, removeNode: true)
         }
         timeline.after(time: 4) {
+            let fadeDuration: CGFloat = 0.5
+            let moveDuration: CGFloat = 2
+
+            let powerupTextNode = TextNode(at: CGPoint(-180, 170))
+            powerupTextNode.text = "NEW POWERUPS!"
+            powerupTextNode.fadeTo(1, start: 0, duration: fadeDuration)
+            self.gameUI << powerupTextNode
+
+            let arrowTextNode = TextNode(at: CGPoint(-270, 140))
+            arrowTextNode.text = "←"
+            arrowTextNode.fadeTo(1, start: 0, duration: fadeDuration)
+            arrowTextNode.moveTo(arrowTextNode.position + CGPoint(y: -90), duration: moveDuration)
+            self.timeline.after(time: moveDuration - fadeDuration) {
+                powerupTextNode.fadeTo(0, duration: fadeDuration, removeNode: true)
+                arrowTextNode.fadeTo(0, duration: fadeDuration, removeNode: true)
+            }
+            self.gameUI << arrowTextNode
+
             super.populateZoomOut()
         }
     }
 
     override func populateLevel() {
-        let powerupTextNode = TextNode(at: CGPoint(-160, 170))
-        powerupTextNode.text = "←"
-        let fadeDuration: CGFloat = 0.5
-        let moveDuration: CGFloat = 2
-        powerupTextNode.fadeTo(1, start: 0, duration: fadeDuration)
-        powerupTextNode.moveTo(powerupTextNode.position + CGPoint(y: -120), duration: moveDuration)
-        timeline.after(time: moveDuration - fadeDuration) {
-            powerupTextNode.fadeTo(0, duration: fadeDuration, removeNode: true)
-        }
-        self << powerupTextNode
-
         // linkWaves(
         //     beginWave1,
         //     beginWave2,
